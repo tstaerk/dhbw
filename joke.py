f7a07316d534dbe458918551c4faddf4b9dfefcf
Thorsten Staerk <thorsten@staerk.de>
2025-03-04 13:27:43 +0100

    next version

--- joke.py
+++ joke.py
@@ -6,9 +6,9 @@ import os
 
 # This program lets AI tell a joke
 # It is useful not only to put a smile on your face, but also to check if you have set up libraries and API keys correctly
+# It expects to find an operating system environment variable API_KEY containing the API key.
 
-st.write(os.environ['API_KEY'])
 ai.configure(api_key=os.environ['API_KEY'])
-model = ai.GenerativeModel("gemini-1.5-pro-002")
+model = ai.GenerativeModel("gemini-2.0-flash-001") # to find out what models are available, go to https://console.cloud.google.com -> vertex AI -> freeform and have a look at the right-hand-side panel which has a selection for "Model"
 
 st.write(model.generate_content("tell a joke").candidates[0].content.parts[0].text)
